04ba8a4c54f63a78c777f66546f22ca64e9a0d83
Himalaya <yugmani@yahoo.com>
2022-06-16 00:42:03 -0700

    addeventlistener to remove an item from the list

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inputEl = document.getElementById("shopping-item");
 const btnEl = document.getElementById("button-add");
 const listEl = document.getElementById("list-items");
-const trashEl = document.querySelectorAll("trash-can");
 
 const items = ["hat", "shoes", "sunglass", "pizza"];
 
-const displayItems = (arr) => {
+window.onload = displayItems(items);
+
+function displayItems(arr) {
   let html = "";
   arr.map((item) => {
     html += `
@@ -18,15 +19,27 @@ const displayItems = (arr) => {
   });
 
   listEl.innerHTML = html;
-};
-
-displayItems(items);
+}
 
 btnEl.addEventListener("click", function (event) {
   event.preventDefault();
   let newItem = inputEl.value.trim();
-  console.log(newItem);
   items.push(newItem);
   displayItems(items);
   inputEl.value = "";
+});
+
+const trashEl = document.querySelectorAll(".trash-can");
+
+trashEl.forEach((trash) => {
+  trash.addEventListener("click", function (e) {
+    const target = e.target;
+    console.log(target);
+    const parentTarget = target.parentElement.parentElement;
+    const contentTarget = parentTarget.textContent.trim();
+    const position = items.indexOf(contentTarget);
+    console.log(target, position);
+    items.splice(position, 1);
+    displayItems(items);
+  });
 });
